a5985154ab2afd7f09a6bc7fcd7a927f9aad77b2
Vanniery Fernandes <vanniery.fernandes@outlook.com>
2023-03-07 08:47:00 -0300

    ifs condicionais

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-	print("Hello world!")
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+
+	if a > b {
+		fmt.Println("A maior que B")
+	} else if b > a {
+		fmt.Println("B maior que A")
+	} else {
+		fmt.Println("São iguais")
+	}
+	// print("Hello world!")
 }
